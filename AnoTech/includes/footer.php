d0d4117dcde8112dd637c593052b23d138950f5e
Prakhar Yadav <pkrc267@users.noreply.github.com>
2017-03-03 15:46:02 +0530

    First iteration front-end
    uploading the front-end in the first iteration.

---
+++ AnoTech/includes/footer.php
@@ -0,0 +1,7 @@
+<footer class="panel-footer">
+    <div class="container">
+        <center>
+            <p><b>Copyright &copy; Lifestyle Store. All Rights Reserved  |  Contact Us: +91 90000 00000</b></p>
+        </center>
+    </div>
+</footer>
