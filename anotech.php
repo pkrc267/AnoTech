b6ff5608e0d51ab5ff89cad891a5a380c6c46ddf
Prakhar Yadav <pkrc267@gmail.com>
2017-03-06 23:51:01 +0530

    adding .html just to display the site in github

--- anotech.php
+++ anotech.php
@@ -6,7 +6,16 @@ if(isset($_POST['upload'])){
   
   exec('python anotech.py', $output);
   print_r($output);
-
 }
 
 ?>
+<!DOCTYPE html>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>anotech</title>
+</head>
+<body>
+	<a href="test.html">click</a>
+</body>
+</html>
